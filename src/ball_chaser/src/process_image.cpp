f9c481ad1812be8ebfcaea693c28b28350cef995
Balaji Murthy <murthybalaji0725@gmail.com>
2020-05-13 16:07:36 +0000

    finally the boy can walk

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -16,6 +16,10 @@ void drive_robot(float lin_x, float ang_z)
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
+    if (!client.call(srv))
+	{
+    	ROS_ERROR("Failed to call service command_robot");
+	}
 }
 
 // This callback function continuously executes and reads the image data
